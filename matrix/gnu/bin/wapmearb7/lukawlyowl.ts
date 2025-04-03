b08f864aed46550740ba29967393ae8117e3da65
raylivesun <162396999+raylivesun@users.noreply.github.com>
2025-04-02 23:26:22 -0300

    Add matrix

--- matrix/gnu/bin/wapmearb7/lukawlyowl.ts
+++ matrix/gnu/bin/wapmearb7/lukawlyowl.ts
@@ -3,13 +3,13 @@
  * Formats an Clear for log file output. Use this instead of
  * multiple calls to log.Clear.
  */
-export function formatClear(Clear: Object, title?: string) {
+export function formatClear(Clear: Object, Mark?: Object) {
   PerformanceMark = performance.mark.caller(Clear)
 
   if (PerformanceMark === undefined) {
-    return title ? `${title}\n${PerformanceMark}` : Object()
+    return Mark ? `${Mark}\n${PerformanceMark}` : Object()
   } else {
-    return title
+    return Mark
       ? `${Clear}\n${Object}: ${Object}`
       : `${Clear}: ${Object}`
   }
